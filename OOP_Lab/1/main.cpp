e85eff5795bc959b66389c8e082c2bfe65f9b515
dannikinfo <danil74808@gmail.com>
2019-03-16 09:03:42 +0700

    Final Lab 1

--- OOP_Lab/1/main.cpp
+++ OOP_Lab/1/main.cpp
@@ -2,122 +2,131 @@
 
 using std::string;
 
-class NumberBook{
-public:
-    string name, lastName;
-    int number;
-    string birthDay;
+class Container{
+    public:
+        class NumberBook{
+        public:
+            string name, lastName;
+            int number;
+            string birthDay;
 
-    NumberBook(string name, string lastName, int number, string birthDay){
-        setName(name);
-        setLastName(lastName);
-        setNumber(number);
-        setBirthDay(birthDay);
-    }
-    NumberBook(){}
+            NumberBook(string name, string lastName, int number, string birthDay){
+                setName(name);
+                setLastName(lastName);
+                setNumber(number);
+                setBirthDay(birthDay);
+            }
+            NumberBook(){}
 
-    void setName(string name){
-        this->name = name;
-    }
+            void setName(string name){
+                this->name = name;
+            }
 
-    void setLastName(string lastName){
-        this->lastName = lastName;
-    }
+            void setLastName(string lastName){
+                this->lastName = lastName;
+            }
 
-    void setNumber(int number) {
-        this->number = number;
-    }
+            void setNumber(int number) {
+                this->number = number;
+            }
 
-    void setBirthDay(string birthDay){
-        this->birthDay = birthDay;
-    }
+            void setBirthDay(string birthDay){
+                this->birthDay = birthDay;
+            }
 
-    string getName(){
-        return name;
-    }
+            string getName(){
+                return name;
+            }
 
-    string getLastName(){
-        return lastName;
-    }
+            string getLastName(){
+                return lastName;
+            }
 
-    int getNumber() {
-        return number;
-    }
+            int getNumber() {
+                return number;
+            }
 
-    string getBirthDay(){
-        return birthDay;
-    }
+            string getBirthDay(){
+                return birthDay;
+            }
+        };
+    private:
+        NumberBook nb[100000];
+    public:
+        NumberBook* getNB(){
+            return nb;
+        }
+        void sort(int realSize, int s){
+            for (int startIndex = 0; startIndex < realSize - 1; ++startIndex){
+                int smallestIndex = startIndex;
+                for (int currentIndex = startIndex + 1; currentIndex < realSize; ++currentIndex){
+                    if(s == 1)
+                        if (nb[currentIndex].getName() < nb[smallestIndex].getName())
+                            smallestIndex = currentIndex;
+                    if(s == 2)
+                        if (nb[currentIndex].getLastName() < nb[smallestIndex].getLastName())
+                            smallestIndex = currentIndex;
+                    if(s == 3)
+                        if (nb[currentIndex].getNumber() < nb[smallestIndex].getNumber())
+                            smallestIndex = currentIndex;
+                    if(s == 4)
+                        if (nb[currentIndex].getBirthDay() < nb[smallestIndex].getBirthDay())
+                            smallestIndex = currentIndex;
+                }
+                std::swap(nb[startIndex], nb[smallestIndex]);
+            }
+        }
+
+        void search(int realSize, int s){
+            int i = 0;
+            if(s == 1){
+                string name;
+                std::cout << "Введите имя" << std::endl;
+                std::cin >> name;
+                for(i = 0; i < realSize; i++){
+                    if(nb[i].getName() == name)
+                        break;
+                }
+            }
+            if(s == 2){
+                string lastName;
+                std::cout << "Введите фамилию" << std::endl;
+                std::cin >> lastName;
+                for(i = 0; i < realSize; i++){
+                    if(nb[i].getLastName() == lastName)
+                        break;
+                }
+            }
+            if(s == 3){
+                int number;
+                std::cout << "Введите номер" << std::endl;
+                std::cin >> number;
+                for(i = 0; i < realSize; i++){
+                    if(nb[i].getNumber() == number)
+                        break;
+                }
+            }
+            if(s == 4){
+                string birthDay;
+                std::cout << "Введите дату рождения (дд.мм.гггг)" << std::endl;
+                std::cin >> birthDay;
+                for(i = 0; i < realSize; i++){
+                    if(nb[i].getBirthDay() == birthDay)
+                        break;
+                }
+            }
+            if(i == realSize)
+                std::cout << "В записной книжке нет такой записи!" << std::endl;
+            else{
+                std::cout << "Имя: Фамилия: Номер телефона: Дата рождения:" << std::endl;
+                std::cout << nb[i].getName() << " " << nb[i].getLastName() << " " << nb[i].getNumber() << " " << nb[i].getBirthDay() << std::endl;
+            }
+        }
 };
-NumberBook nb[1000];
-
-void sort(int realSize, int s){
-    for (int startIndex = 0; startIndex < realSize - 1; ++startIndex){
-        int smallestIndex = startIndex;
-        for (int currentIndex = startIndex + 1; currentIndex < realSize; ++currentIndex){
-            if(s == 1)
-                if (nb[currentIndex].getName() < nb[smallestIndex].getName())
-                    smallestIndex = currentIndex;
-            if(s == 2)
-                if (nb[currentIndex].getLastName() < nb[smallestIndex].getLastName())
-                    smallestIndex = currentIndex;
-            if(s == 3)
-                if (nb[currentIndex].getNumber() < nb[smallestIndex].getNumber())
-                    smallestIndex = currentIndex;
-            if(s == 4)
-                if (nb[currentIndex].getBirthDay() < nb[smallestIndex].getBirthDay())
-                    smallestIndex = currentIndex;
-        }
-        std::swap(nb[startIndex], nb[smallestIndex]);
-    }
-}
-
-void search(int realSize, int s){
-    int i = 0;
-    if(s == 1){
-        string name;
-        std::cout << "Введите имя" << std::endl;
-        std::cin >> name;
-        for(i = 0; i < realSize; i++){
-            if(nb[i].getName() == name)
-                break;
-        }
-    }
-    if(s == 2){
-        string lastName;
-        std::cout << "Введите фамилию" << std::endl;
-        std::cin >> lastName;
-        for(i = 0; i < realSize; i++){
-            if(nb[i].getLastName() == lastName)
-                break;
-        }
-    }
-    if(s == 3){
-        int number;
-        std::cout << "Введите номер" << std::endl;
-        std::cin >> number;
-        for(i = 0; i < realSize; i++){
-            if(nb[i].getNumber() == number)
-                break;
-        }
-    }
-    if(s == 4){
-        string birthDay;
-        std::cout << "Введите дату рождения (дд.мм.гггг)" << std::endl;
-        std::cin >> birthDay;
-        for(i = 0; i < realSize; i++){
-            if(nb[i].getBirthDay() == birthDay)
-                break;
-        }
-    }
-    if(i == realSize)
-        std::cout << "В записной книжке нет такой записи!" << std::endl;
-    else{
-        std::cout << "Имя: Фамилия: Номер телефона: Дата рождения:" << std::endl;
-        std::cout << nb[i].getName() << " " << nb[i].getLastName() << " " << nb[i].getNumber() << " " << nb[i].getBirthDay() << std::endl;
-    }
-}
 
 int main() {
+    Container cont;
+    Container::NumberBook* nb = cont.getNB();
     int realSize = 0;
     int k = 10;
     while(k != 0){
@@ -156,14 +165,14 @@ int main() {
                 std::cout << "Выберите поле сортировки: 1-имя 2-фамилия 3-номер телефона 4-дата рождения" << std::endl;
                 int s;
                 std::cin >> s;
-                sort(realSize, s);
+                cont.sort(realSize, s);
                 std::cout << "Сортировка завершена" << std::endl;
                 break;
             case 5:
                 std::cout << "Выберите поле для поиска: 1-имя 2-фамилия 3-номер телефона 4-дата рождения" << std::endl;
                 int b;
                 std::cin >> b;
-                search(realSize, b);
+                cont.search(realSize, b);
                 std::cout << "Поиск завершен!" << std::endl;
                 break;
             case 1:
@@ -173,7 +182,7 @@ int main() {
                 std::cout<<"Введите: Имя Фамилию Номер Дату рождения(ДД.ММ.ГГГГ)"<<std::endl;
                 std::cin>>name>>lastName>>number>>birthDay;
                 std::cout<<std::endl;
-                nb[realSize++] = NumberBook(name,lastName,number,birthDay);
+                nb[realSize++] = Container::NumberBook(name,lastName,number,birthDay);
                 break;
         }
     }
